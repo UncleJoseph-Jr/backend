dc956eae3b55690a066e5bc007e9c8d7a220bace
UncleJoseph-Jr <141295348+UncleJoseph-Jr@users.noreply.github.com>
2024-12-27 19:27:44 +0700

    Create file-upload.service.ts

---
+++ src/products/file-upload.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import {
+  MulterOptionsFactory,
+  MulterModuleOptions,
+} from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+@Injectable()
+export class FileUploadService implements MulterOptionsFactory {
+  createMulterOptions(): MulterModuleOptions {
+    return {
+      storage: diskStorage({
+        destination: './uploads/products',
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          cb(null, `${uniqueSuffix}${ext}`);
+        },
+      }),
+    };
+  }
+}
